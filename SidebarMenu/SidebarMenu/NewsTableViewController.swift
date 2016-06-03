e6622c3b291a1fe422653f1b6fb6847dec537420
Andy Miller <andymill@gmail.com>
2016-06-03 14:13:18 -0700

    ui refactor done

--- SidebarMenu/SidebarMenu/NewsTableViewController.swift
+++ SidebarMenu/SidebarMenu/NewsTableViewController.swift
@@ -42,22 +42,22 @@ class NewsTableViewController: UITableViewController {
     
     // Configure the cell...
     if indexPath.row == 0 {
-      cell.postImageView.image = UIImage(named: "watchkit-intro")
-      cell.postTitleLabel.text = "Social Feed Item I"
+      cell.postImageView.image = UIImage(named: "inky")
+      cell.postTitleLabel.text = "New Zealand Aquarium Octopus Has Flown The Coop"
       cell.authorLabel.text = "Jane Doe"
-      cell.authorImageView.image = UIImage(named: "author")
+      cell.authorImageView.image = UIImage(named: "userpic")
       
     } else if indexPath.row == 1 {
-      cell.postImageView.image = UIImage(named: "custom-segue-featured-1024")
-      cell.postTitleLabel.text = "Building a Chat App in Swift Using Multipeer Connectivity Framework"
+      cell.postImageView.image = UIImage(named: "coffee_table_aquarium")
+      cell.postTitleLabel.text = "We love this aquarium here at Demo App"
       cell.authorLabel.text = "App Name"
-      cell.authorImageView.image = UIImage(named: "tote.png")
+      cell.authorImageView.image = UIImage(named: "tote")
       
     } else {
-      cell.postImageView.image = UIImage(named: "webkit-featured")
-      cell.postTitleLabel.text = "A Beginner’s Guide to Animated Custom Segues in iOS 8"
+      cell.postImageView.image = UIImage(named: "whaleshark")
+      cell.postTitleLabel.text = "Whale Shark Does Barrel Roll to Avoid Sea Turtle Off Australia's Gold Coast"
       cell.authorLabel.text = "Jane Doe"
-      cell.authorImageView.image = UIImage(named: "appcoda-300")
+      cell.authorImageView.image = UIImage(named: "userpic")
       
     }
     
